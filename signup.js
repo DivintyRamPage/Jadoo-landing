e6892b4a4fa25623cbe1edd7869953feb48fd292
DivintyRamPage <mogirvitala@gmail.com>
2026-07-21 21:25:59 +0300

    make log in

--- signup.js
+++ signup.js
@@ -9,9 +9,14 @@ signUpWindowJs.addEventListener('click', () => {
     signUpWindow.classList.remove('sign-up-window-visible');
 });
 
-const loginBtn = document.querySelector('.btn-login'); 
-const loginWindow = document.querySelector('.js-login-window');
-loginBtn.addEventListener('click', () => {
-    loginWindow.classList.add('log-in-window-visible');
+const logInBtn = document.querySelector('.btn-login'); 
+const logInWindow = document.querySelector('.js-login-window');
+logInBtn.addEventListener('click', () => {
+    logInWindow.classList.add('log-in-window-visible');
+});
+
+const logInWindowJs = document.querySelector('.js-login-close');
+logInWindowJs.addEventListener('click', () => {
+    logInWindow.classList.remove('log-in-window-visible');
 });
 
